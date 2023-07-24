411171cda560ac01c54f66e6d44dbbe528ee2d9d
n-dub <ragucoz@gmail.com>
2023-07-25 00:06:46 +0500

    Add `Result<T>` API

---
+++ UraniumDI/UnDI/Utils/ErrorCode.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <cassert>
+#include <cstdint>
+
+namespace UN
+{
+    //! \brief Represents a general error result of a function call within the library.
+    //!
+    //! Different functions may have their own error codes, but this enum should be enough for a general case.
+    enum class ErrorCode : uint32_t
+    {
+        Fail,             //!< Operation failed.
+        Abort,            //!< Operation aborted.
+        NotImplemented,   //!< Operation not implemented.
+        InvalidOperation, //!< Operation was invalid.
+        InvalidArguments, //!< One or more arguments were invalid.
+        AccessDenied,     //!< General access denied error occurred.
+        Timeout,          //!< Operation timed out.
+        OutOfMemory       //!< Not enough memory to complete the operation.
+    };
+
+    //! \brief Get a constant string that represents an ErrorCode enum value.
+    inline const char* ErrorCodeToString(ErrorCode result)
+    {
+        switch (result)
+        {
+            // clang-format off
+        case ErrorCode::Fail: return "ErrorCode::Fail";
+        case ErrorCode::Abort: return "ErrorCode::Abort";
+        case ErrorCode::NotImplemented: return "ErrorCode::NotImplemented";
+        case ErrorCode::InvalidArguments: return "ErrorCode::InvalidArguments";
+        case ErrorCode::InvalidOperation: return "ErrorCode::InvalidOperation";
+        case ErrorCode::AccessDenied: return "ErrorCode::AccessDenied";
+        case ErrorCode::Timeout: return "ErrorCode::Timeout";
+        case ErrorCode::OutOfMemory: return "ErrorCode::OutOfMemory";
+            // clang-format on
+        default:
+            assert(false && "ErrorCode was unknown");
+            return "ErrorCode::<Unknown>";
+        }
+    }
+} // namespace UN
